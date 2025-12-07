bc34ebccfb595478f8db2154c3c84d2478031a29
Nights-SDH <sdhqaa@kaist.ac.kr>
2025-12-07 22:42:28 +0900

    버그 수정

--- Assets/teams/team_3/Scripts/05_Game/03_UserTest/00_System/MRUKLaserInteractor.cs
+++ Assets/teams/team_3/Scripts/05_Game/03_UserTest/00_System/MRUKLaserInteractor.cs
@@ -110,7 +110,7 @@ public class MRUKLaserInteractor : MonoBehaviour
                 {
                     // 로컬 X, Y 좌표 전송
                     NetworkManagerPython.Instance.SendCardMove(
-                        cardId, 
+                        hit.collider.GetComponent<NetworkCard>().cardID, 
                         new Vector2(localPos.x, localPos.y)
                     );
                 }
