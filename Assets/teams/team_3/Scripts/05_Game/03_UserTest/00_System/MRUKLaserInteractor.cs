13e3a22bca6a83508591a38255360df6d0d2ee53
Nights-SDH <sdhqaa@kaist.ac.kr>
2025-12-07 21:56:42 +0900

    Card Drag 횟수 추적

--- Assets/teams/team_3/Scripts/05_Game/03_UserTest/00_System/MRUKLaserInteractor.cs
+++ Assets/teams/team_3/Scripts/05_Game/03_UserTest/00_System/MRUKLaserInteractor.cs
@@ -18,6 +18,7 @@ public class MRUKLaserInteractor : MonoBehaviour
     private NetworkCard hoveredCard = null;
     private NetworkCard selectedObject = null;
     private bool isDragging = false;
+    private int count = 0;
 
     // [추가] 네트워크 전송 빈도 조절용 변수
     private float lastSendTime = 0f;
@@ -88,10 +89,10 @@ public class MRUKLaserInteractor : MonoBehaviour
         if (Physics.Raycast(ray, out hit, maxDistance, canvasLayer))
         {
             // 1. 시각적 이동 (World Space)
-            Vector3 targetPos = hit.point + (hit.normal * 0.02f);
+            Vector2 targetPos = hit.point + (hit.normal * 0.02f);
             
             // 부드러운 이동
-            selectedObject.transform.position = Vector3.Lerp(selectedObject.transform.position, targetPos, Time.deltaTime * 20f);
+            selectedObject.transform.position = Vector3.Lerp(selectedObject.transform.position, new Vector3(targetPos.x, targetPos.y, selectedObject.transform.position.z), Time.deltaTime * 20f);
 
             // 레이저 길이 조절
             SetLaserLength(hit.distance);
@@ -149,5 +150,7 @@ public class MRUKLaserInteractor : MonoBehaviour
         if (selectedObject != null) selectedObject.OnRelease();
         selectedObject = null;
         isDragging = false;
+        count+=1;
+        Debug.Log($"카드 드래그 {count}");
     }
 }
