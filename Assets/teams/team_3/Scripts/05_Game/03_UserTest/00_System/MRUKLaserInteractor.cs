d454e2605aad4b836e89fea52935d2600e75555a
Nights-SDH <sdhqaa@kaist.ac.kr>
2025-12-07 22:19:28 +0900

    Card Canvas 겹치면 문제있는 버그 수정

--- Assets/teams/team_3/Scripts/05_Game/03_UserTest/00_System/MRUKLaserInteractor.cs
+++ Assets/teams/team_3/Scripts/05_Game/03_UserTest/00_System/MRUKLaserInteractor.cs
@@ -89,10 +89,18 @@ public class MRUKLaserInteractor : MonoBehaviour
         if (Physics.Raycast(ray, out hit, maxDistance, canvasLayer))
         {
             // 1. 시각적 이동 (World Space)
-            Vector2 targetPos = hit.point + (hit.normal * 0.02f);
+            Vector3 targetPos = hit.point + (hit.normal * 0.02f);
+
+            Vector3 finalPos = new Vector3(
+                targetPos.x,
+                targetPos.y,
+                selectedObject.transform.position.z // Z축은 고정
+            );
+
+            Debug.Log(finalPos);
             
             // 부드러운 이동
-            selectedObject.transform.position = Vector3.Lerp(selectedObject.transform.position, new Vector3(targetPos.x, targetPos.y, selectedObject.transform.position.z), Time.deltaTime * 20f);
+            selectedObject.transform.position = Vector3.Lerp(selectedObject.transform.position, finalPos, Time.deltaTime * 20f);
 
             // 레이저 길이 조절
             SetLaserLength(hit.distance);
