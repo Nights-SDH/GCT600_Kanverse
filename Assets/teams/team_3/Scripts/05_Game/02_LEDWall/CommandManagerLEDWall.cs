616af1a3abe18a7d92f1890e23e8c23b11bad00e
Nights-SDH <sdhqaa@kaist.ac.kr>
2025-12-10 18:30:28 +0900

    Update

--- Assets/teams/team_3/Scripts/05_Game/02_LEDWall/CommandManagerLEDWall.cs
+++ Assets/teams/team_3/Scripts/05_Game/02_LEDWall/CommandManagerLEDWall.cs
@@ -2,23 +2,23 @@ using UnityEngine;
 
 public class CommandManagerLEDWall: MonoBehaviour
 {
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TitleManager.Instance.OnStartButtonClicked(); 
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            LobbyManager.Instance.AddParticipant();
-        }
-        if (Input.GetKeyDown(KeyCode.RightShift))
-        {
-            LobbyManager.Instance.OnStartButtonClicked();
-        }
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            DialogManager.Instance.CommandCheck();
-        }
-    }
+    // void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.Space))
+    //     {
+    //         TitleManager.Instance.OnStartButtonClicked(); 
+    //     }
+    //     if (Input.GetKeyDown(KeyCode.Alpha1))
+    //     {
+    //         LobbyManager.Instance.AddParticipant();
+    //     }
+    //     if (Input.GetKeyDown(KeyCode.RightShift))
+    //     {
+    //         LobbyManager.Instance.OnStartButtonClicked();
+    //     }
+    //     if (Input.GetKeyDown(KeyCode.LeftShift))
+    //     {
+    //         DialogManager.Instance.CommandCheck();
+    //     }
+    // }
 }
